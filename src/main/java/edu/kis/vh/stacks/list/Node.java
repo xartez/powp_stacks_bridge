162778b454f52b8a6f00757f436e4eb96b72e0c9
Jakub Chludziński <jakubchludzinski@gmail.com>
2017-03-02 22:28:02 +0100

    Realizacja punktu 8 - enkapsulacja atrybutów w klasie node

--- src/main/java/edu/kis/vh/stacks/list/Node.java
+++ src/main/java/edu/kis/vh/stacks/list/Node.java
@@ -2,11 +2,36 @@ package edu.kis.vh.stacks.list;
 
 public class Node {
 
-	public int value;
-	public Node prev, next;
+	private int value; // pozostale klasy korzystajace z atrybutu zaczely odnosić się do niego za pomocą getterów i setterów
+	private Node prev; // jw.
+	private Node next; // jw.
 
 	public Node(int i) {
 		value = i;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	public Node getPrev() {
+		return prev;
+	}
+
+	public void setPrev(Node prev) {
+		this.prev = prev;
 	}
 
 }
