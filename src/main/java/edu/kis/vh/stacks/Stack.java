48bc39712ad3786eeb7285b5cd591993d637c4f6
Jakub Chludziński <jakubchludzinski@gmail.com>
2017-03-02 22:16:53 +0100

    Realizacja punktu 7 - wygenerowanie gettera i settera dla zmiennej total

--- src/main/java/edu/kis/vh/stacks/Stack.java
+++ src/main/java/edu/kis/vh/stacks/Stack.java
@@ -13,6 +13,14 @@ public class Stack {
 	private int total = EMPTY_STACK_VALUE;
 	
 
+	public int getTotal() {
+		return total;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+
 	public void push(int i) {
 		if (!isFull())
 			items[++total] = i;
