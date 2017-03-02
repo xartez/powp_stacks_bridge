10cbfbe171440d17b4f52e9069214097696ed721
xartez <jakubchludzinski@gmail.com>
2017-03-02 15:36:38 +0100

    Punkt 5 wydzielenie stałych

--- src/main/java/edu/kis/vh/stacks/Stack.java
+++ src/main/java/edu/kis/vh/stacks/Stack.java
@@ -2,9 +2,16 @@ package edu.kis.vh.stacks;
 
 public class Stack {
 
-	private int[] items = new int[12];
+	private static final int FULL_STACK_VALUE = 11;
 
-	public int total = -1;
+	private static final int STACK_SIZE_VALUE = 12;
+
+	private static final int EMPTY_STACK_VALUE = -1;
+
+	private int[] items = new int[STACK_SIZE_VALUE];
+
+	public int total = EMPTY_STACK_VALUE;
+	
 
 	public void push(int i) {
 		if (!isFull())
@@ -12,22 +19,22 @@ public class Stack {
 	}
 
 	public boolean isEmpty() {
-		return total == -1;
+		return total == EMPTY_STACK_VALUE;
 	}
 
 	public boolean isFull() {
-		return total == 11;
+		return total == FULL_STACK_VALUE;
 	}
 
 	public int top() {
 		if (isEmpty())
-			return -1;
+			return EMPTY_STACK_VALUE;
 		return items[total];
 	}
 
 	public int pop() {
 		if (isEmpty())
-			return -1;
+			return EMPTY_STACK_VALUE;
 		return items[total--];
 	}
 
