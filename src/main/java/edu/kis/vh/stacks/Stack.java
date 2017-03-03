b7ec77b75617253823c77e7625097ee9b2c013c5
Jakub Chludziński <jakubchludzinski@gmail.com>
2017-03-03 22:17:14 +0100

    3.3.2 dodanie dokumentacji klasy Stack

--- src/main/java/edu/kis/vh/stacks/Stack.java
+++ src/main/java/edu/kis/vh/stacks/Stack.java
@@ -1,5 +1,30 @@
 package edu.kis.vh.stacks;
 
+/**
+ * @author Jakub
+ *
+ * public int getTotal()
+ * 	zwraca liczbe elementow przechowywanych na stosie
+ * 
+ * public void push(int i)
+ * 	odpowiada za dodanie elementu przekazanym przez parametr do stosu
+ * 
+ * public boolean isEmpty()
+ * 	sprawdza czy stos przechowuje jakies elementy, jezeli stos jest pusty zwraca EMPTY_STACK_VALUE = -1
+ * 
+ * public boolean isFull() 
+ *	sprawdza czy stos jest pełny, jeżeli stos jest pełen zwraca FULL_STACK_VALUE = 11
+ *
+ * public int top() 
+ * 	zwraca element ze szczytu listy, jezeli stos jest pusty EMPTY_STACK_VALUE = -1
+ * 
+ * public int pop()
+ *	uswa element ze stosu i zwraca tablice bez usuniętego elementu, jezeli stos jest pusty EMPTY_STACK_VALUE = -1
+ *
+ *
+ * Generalnie dażymy do tego by kod był na tyle czytelny aby nie było konieczności pisać takiej zbednej dokumentacji.
+ * 
+ */
 public class Stack {
 
 	private static final int FULL_STACK_VALUE = 11;
