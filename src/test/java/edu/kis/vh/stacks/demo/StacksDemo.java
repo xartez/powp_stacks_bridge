887c4b13e7fbccd7e159d779e82e1e2df5de1deb
Jakub Chludziński <jakubchludzinski@gmail.com>
2017-03-03 18:19:12 +0100

    Realizacja punktu 13 - ekstrakcja zawartosci metody main do oddzielnej metody

--- src/test/java/edu/kis/vh/stacks/demo/StacksDemo.java
+++ src/test/java/edu/kis/vh/stacks/demo/StacksDemo.java
@@ -9,7 +9,10 @@ class StacksDemo {
 
 	public static void main(String[] args) {
 		DefaultStacksFactory factory = new DefaultStacksFactory();
+		testStacks(factory);
+	}
 
+	private static void testStacks(DefaultStacksFactory factory) {
 		Stack[] stacks = { factory.getStandardStack(), factory.getFalseStack(), factory.getFIFOStack(),
 				factory.getHanoiStack() };
 
@@ -28,7 +31,6 @@ class StacksDemo {
 		}
 
 		System.out.println("total rejected is " + ((StackHanoi) stacks[3]).reportRejected());
-
 	}
 
 }
