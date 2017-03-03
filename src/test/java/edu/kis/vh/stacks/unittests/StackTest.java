1157da284246114f606c141e5076f4dc03212bf5
Jakub Chludziński <jakubchludzinski@gmail.com>
2017-03-03 19:17:03 +0100

    Punkt 3.2 wykonanie i uzupełnienie testów

--- src/test/java/edu/kis/vh/stacks/unittests/StackTest.java
+++ src/test/java/edu/kis/vh/stacks/unittests/StackTest.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import edu.kis.vh.stacks.Stack;
+import edu.kis.vh.stacks.StackFIFO;
+import edu.kis.vh.stacks.StackHanoi;
 
 // kombinacja alt + <- i alt + -> kieruje cie kolejna wstecz lub do przodu ostatnio odwiedzonych linii w kazdym pliku  
 public class StackTest {
@@ -77,5 +79,33 @@ public class StackTest {
 		result = stackObj.pop();
 		Assert.assertEquals(EMPTY_STACK_VALUE, result);
 	}
+	
+	@Test
+	public void testFIFOPop() {
+		StackFIFO stackObj = new StackFIFO();
+		final int EMPTY_STACK_VALUE = -1;
+
+		int result = stackObj.pop();
+		Assert.assertEquals(EMPTY_STACK_VALUE, result);
+		
+		int testValue = 4;
+		stackObj.push(testValue);
+
+		result = stackObj.pop();
+		Assert.assertEquals(testValue, result);
+		result = stackObj.pop();
+		Assert.assertEquals(EMPTY_STACK_VALUE, result);
+	}
+	
+	@Test
+	public void testHanoiPush() {
+		StackHanoi stackObj = new StackHanoi();
+		int testValue = 4;
+		stackObj.push(testValue);
+
+		int result = stackObj.top();
+		Assert.assertEquals(testValue, result);
+	}
+
 
 }
