32b101e5015a94500f5b7c919583388569c27c6c
Jakub Chludziński <jakubchludzinski@gmail.com>
2017-03-22 23:06:32 +0100

    3.2.2

--- src/test/java/edu/kis/vh/stacks/unittests/StackTest.java
+++ src/test/java/edu/kis/vh/stacks/unittests/StackTest.java
@@ -8,58 +8,63 @@ import edu.kis.vh.stacks.Stack;
 import edu.kis.vh.stacks.StackFIFO;
 import edu.kis.vh.stacks.StackHanoi;
 import edu.kis.vh.stacks.factory.ArrayStacksFactory;
+import edu.kis.vh.stacks.factory.StackListFactory;
 
 // kombinacja alt + <- i alt + -> kieruje cie kolejna wstecz lub do przodu ostatnio odwiedzonych linii w kazdym pliku  
 public class StackTest {
 
 	@Test
-	public void testPush() {
-		Stack stackObj = new Stack();
+	public void testStackArrayPush() {
+		ArrayStacksFactory arrayStacksFactory = new ArrayStacksFactory();
+		Stack stackObj = arrayStacksFactory.getStandardStack();
 		int testValue = 4;
 		stackObj.push(testValue);
-
+		
 		int result = stackObj.top();
 		Assert.assertEquals(testValue, result);
 	}
 
 	@Test
-	public void testIsEmpty() {
-		Stack stackObj = new Stack();
-		boolean result = stackObj.isEmpty();
+	public void testStackArrayIsEmpty() {
+		ArrayStacksFactory arrayStacksFactory = new ArrayStacksFactory();
+		Stack stackObj = arrayStacksFactory.getStandardStack();
+		boolean result = stackObj.isEmpty();		
 		Assert.assertEquals(true, result);
-
+		
 		stackObj.push(888);
-
+		
 		result = stackObj.isEmpty();
 		Assert.assertEquals(false, result);
 	}
 
 	@Test
-	public void testIsFull() {
+	public void testStackArrayIsFull() {
 		ArrayStacksFactory arrayStacksFactory = new ArrayStacksFactory();
 		Stack stackObj = arrayStacksFactory.getStandardStack();
 		final int STACK_CAPACITY = 12;
 		for (int i = 0; i < STACK_CAPACITY; i++) {
-			boolean result = stackObj.isFull();
+			boolean result = stackObj.isFull();		
 			Assert.assertEquals(false, result);
 			stackObj.push(888);
 		}
-		// test sprawdzi dzialanie metody isFull() dla stosu implementujacego StackArray
+		
 		boolean result = stackObj.isFull();
 		Assert.assertEquals(true, result);
+		// zmiany w tescie: test sprawdza dzialanie metody isFull() dla stosu implementujacego StackArray
 	}
 
 	@Test
-	public void testTop() {
-		Stack stackObj = new Stack();
+	public void testStackArrayTop() {
+		ArrayStacksFactory arrayStacksFactory = new ArrayStacksFactory();
+		Stack stackObj = arrayStacksFactory.getStandardStack();
 		final int EMPTY_STACK_VALUE = -1;
-
+		
 		int result = stackObj.top();
 		Assert.assertEquals(EMPTY_STACK_VALUE, result);
-
+		
 		int testValue = 4;
 		stackObj.push(testValue);
-
+		
 		result = stackObj.top();
 		Assert.assertEquals(testValue, result);
 		result = stackObj.top();
@@ -67,21 +72,24 @@ public class StackTest {
 	}
 
 	@Test
-	public void testPop() {
-		Stack stackObj = new Stack();
+	public void testStackArrayPop() {
+		ArrayStacksFactory arrayStacksFactory = new ArrayStacksFactory();
+		Stack stackObj = arrayStacksFactory.getStandardStack();
 		final int EMPTY_STACK_VALUE = -1;
-
+		
 		int result = stackObj.pop();
 		Assert.assertEquals(EMPTY_STACK_VALUE, result);
-
+		
 		int testValue = 4;
 		stackObj.push(testValue);
-
+		
 		result = stackObj.pop();
 		Assert.assertEquals(testValue, result);
 		result = stackObj.pop();
 		Assert.assertEquals(EMPTY_STACK_VALUE, result);
-	}
+}
+	
+
 	
 	@Test
 	public void testFIFOPop() {
@@ -111,4 +119,81 @@ public class StackTest {
 	}
 // zmiana stalej wskazujacej, ze stos jest pusty (podpunkt 11) spowodowala zakonczenie niektorych testow niepowodzeniem 
 
+	
+	@Test
+	public void testStackListPush() {
+		StackListFactory stackListFactory = new StackListFactory();
+		Stack stackObj = stackListFactory.getStandardStack();
+		int testValue = 4;
+		stackObj.push(testValue);
+		
+		int result = stackObj.top();
+		Assert.assertEquals(testValue, result);
+	}
+
+	@Test
+	public void testStackListIsEmpty() {
+		StackListFactory stackListFactory = new StackListFactory();
+		Stack stackObj = stackListFactory.getStandardStack();
+		boolean result = stackObj.isEmpty();		
+		Assert.assertEquals(true, result);
+		
+		stackObj.push(888);
+		
+		result = stackObj.isEmpty();
+		Assert.assertEquals(false, result);
+	}
+	
+	@Test
+	public void testStackListIsFull() {
+		StackListFactory stackListFactory = new StackListFactory();
+		Stack stackObj = stackListFactory.getStandardStack();
+		final int TEST_STACKLIST_SIZE = 100;
+		
+		boolean result = stackObj.isFull();		
+		Assert.assertEquals(false, result);
+		
+		for (int i = 0; i < TEST_STACKLIST_SIZE; i++) {
+			stackObj.push(888);
+		}
+		
+		result = stackObj.isFull();
+		Assert.assertEquals(false, result);
+	}
+	
+	@Test
+	public void testStackListTop() {
+		StackListFactory stackListFactory = new StackListFactory();
+		Stack stackObj = stackListFactory.getStandardStack();
+		final int EMPTY_STACK_VALUE = -1;
+		
+		int result = stackObj.top();
+		Assert.assertEquals(EMPTY_STACK_VALUE, result);
+		
+		int testValue = 4;
+		stackObj.push(testValue);
+		
+		result = stackObj.top();
+		Assert.assertEquals(testValue, result);
+		result = stackObj.top();
+		Assert.assertEquals(testValue, result);
+	}
+
+	@Test
+	public void testStackListPop() {
+		StackListFactory stackListFactory = new StackListFactory();
+		Stack stackObj = stackListFactory.getStandardStack();
+		final int EMPTY_STACK_VALUE = -1;
+		
+		int result = stackObj.pop();
+		Assert.assertEquals(EMPTY_STACK_VALUE, result);
+		
+		int testValue = 4;
+		stackObj.push(testValue);
+		
+		result = stackObj.pop();
+		Assert.assertEquals(testValue, result);
+		result = stackObj.pop();
+		Assert.assertEquals(EMPTY_STACK_VALUE, result);
+}
 }
