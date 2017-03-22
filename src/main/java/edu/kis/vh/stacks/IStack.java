2f9e009fff79dddf70a4cf5047beb475a7c55e8f
Jakub Chludziński <jakubchludzinski@gmail.com>
2017-03-22 21:54:18 +0100

    3.1.9

--- src/main/java/edu/kis/vh/stacks/IStack.java
+++ src/main/java/edu/kis/vh/stacks/IStack.java
@@ -2,6 +2,8 @@ package edu.kis.vh.stacks;
 
 public interface IStack {
 
+	public static final int EMPTY_STACK_INDICATOR = -1;
+	
 	void push(int i);
 
 	boolean isEmpty();
--- src/main/java/edu/kis/vh/stacks/implementation/StackArray.java
+++ src/main/java/edu/kis/vh/stacks/implementation/StackArray.java
@@ -1,4 +1,4 @@
-package edu.kis.vh.stacks.list;
+package edu.kis.vh.stacks.implementation;
 
 import edu.kis.vh.stacks.IStack;
 
